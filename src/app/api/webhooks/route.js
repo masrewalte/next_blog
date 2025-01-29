2df2e992e67a27075aa6eb6dc35dade4d9228dce
Masre Walte <masrewalte2013@gmail.com>
2025-01-29 22:03:42 +0300

    fix: webhook 1

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,4 +1,5 @@
-import { Webhook } from "svix";
+// import { Webhook } from "svix";
+import { Webhook } from "@clerk/nextjs/api";
 import { headers } from "next/headers";
 import { createOrUpdateUser, deleteUser } from "@/lib/actions/user";
 import { clerkClient } from "@clerk/nextjs/dist/types/server";
